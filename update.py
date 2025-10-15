722ae1d470e14091e5d1cb81a7e951fceb2e094f
Fyaz Mohammed <107264854+fyaz05@users.noreply.github.com>
2025-10-16 01:36:51 +0530

    patch1

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-from os import path as opath, getenv
+from os import path as opath, getenv, rename
 from subprocess import run as srun
 from dotenv import load_dotenv
 from Thunder.utils.logger import logger
@@ -8,22 +8,31 @@ load_dotenv('config.env', override=True)
 UPSTREAM_REPO = getenv('UPSTREAM_REPO', "https://github.com/fyaz05/FileToLink")
 UPSTREAM_BRANCH = getenv('UPSTREAM_BRANCH', "main")
 
-if UPSTREAM_REPO is not None:
-    if opath.exists('.git'):
-        srun(["rm", "-rf", ".git"])
+if UPSTREAM_REPO:
+    config_backup = '../config.env.tmp'
     
-    update = srun([
-        f"git init -q "
-        f"&& git config --global user.email thunder@update.local "
-        f"&& git config --global user.name Thunder "
-        f"&& git add . "
-        f"&& git commit -sm update -q "
-        f"&& git remote add origin {UPSTREAM_REPO} "
-        f"&& git fetch origin -q "
-        f"&& git reset --hard origin/{UPSTREAM_BRANCH} -q"
-    ], shell=True)
-    
-    if update.returncode == 0:
-        logger.info('Successfully updated with latest commit from UPSTREAM_REPO')
-    else:
-        logger.error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+    try:
+        if opath.exists('config.env'):
+            rename('config.env', config_backup)
+        
+        if opath.exists('.git'):
+            srun(["rm", "-rf", ".git"])
+        
+        git_commands = (
+            f"git init -q && "
+            f"git config --global user.email thunder@update.local && "
+            f"git config --global user.name Thunder && "
+            f"git add . && "
+            f"git commit -sm update -q && "
+            f"git remote add origin {UPSTREAM_REPO} && "
+            f"git fetch origin -q && "
+            f"git reset --hard origin/{UPSTREAM_BRANCH} -q"
+        )
+        
+        update = srun(git_commands, shell=True)
+        
+        logger.info('Successfully updated' if update.returncode == 0 else 'Update failed, check UPSTREAM_REPO')
+            
+    finally:
+        if opath.exists(config_backup):
+            rename(config_backup, 'config.env')
